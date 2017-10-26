b42c865c636539ce377d30d42425acede37be78e
G S Sridhar <sridhar.cse12@gmail.com>
2017-10-27 01:56:58 +0530

    added file

---
+++ javascript-is-fun/there-you-go.js
@@ -0,0 +1,9 @@
+(function () { //iife
+    console.log("hi");
+    // defer execution of code
+    //settimeout to 0 puts it in event queue which is executed after call stack is clear.
+    setTimeout(function () {
+        console.log("deferred");
+    }, 0);
+    console.log("u got me");
+})()
